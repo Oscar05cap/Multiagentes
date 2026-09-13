7e2a76610a0c36869b7451f33e73dd923eb204a3
Oscar05cap <odasm.oscar@gmail.com>
2026-09-13 12:41:17 -0600

    Opciones

---
+++ Zumba/src/zumba/Aspiradora.java
@@ -0,0 +1,125 @@
+package zumba;
+
+import jade.core.Agent;
+import jade.core.behaviours.TickerBehaviour;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+import javax.swing.*;
+import java.util.Random;
+
+/**
+ *
+ * @author macuache
+ */
+public class Aspiradora extends Agent
+{
+    private Escenario gui;
+    private  int x = 0, y = 0;  // Posición inicial en la cuadrícula
+    private final int size = 15;     // Tamaño de la habitación
+    private final int dirtRate = 15; // Porcentaje de suciedad
+    private int dir = 0;
+    private final ImageIcon high = new ImageIcon("Zumba/imagenes/buttercup.png");
+    private final ImageIcon medium = new ImageIcon("Zumba/imagenes/bubbles.png");
+    private final ImageIcon low = new ImageIcon("Zumba/imagenes/blossom.png");
+    private final ImageIcon dead = new ImageIcon("Zumba/imagenes/skull.png");
+    private ImageIcon face;
+    private final int initialEnergy = 500;
+    private int energy;
+    private final int tick = 50; // 10 milisegundos 
+    private static int stationRow = -1;
+    private static int stationColum = -1;
+    private final Random aleatorio = new Random();
+    private boolean moving = false;
+    private int matrix[][];
+
+    protected void setup() 
+    {
+        face = high;
+        energy = initialEnergy;
+        gui = Escenario.getInstance(size, dirtRate); // Obtiene instancia del escenario
+        gui.setVisible(true);
+        gui.colocar(0,0, face);
+
+        // Agrega un comportamiento cíclico
+  
+        this.addBehaviour(new CyclicBehaviour(this)
+        {
+           @Override
+           public void action()
+           {
+               ACLMessage msg = receive();  // Espera un mensaje
+               if(msg!=null) // Verifica si se recibió mensaje
+               {
+                   String content = msg.getContent().toLowerCase();  // Extrae el contenido del mensaje
+                   if(content.equals("start") || content.equals("continue")) moving = true;
+                   if(content.equals("stop") || content.equals("pause")) moving = false;
+               }
+           }
+        });
+                
+        // Agrega un comportamiento controlado por tiempo
+        
+        this.addBehaviour(new TickerBehaviour(this, tick) { 
+            @Override
+            protected void onTick() {
+                mover();
+            }
+        });
+    }
+
+    private void mover() 
+    {    
+        if(moving)
+        {
+            // Calcula dirección
+        
+            int yPre = y;
+            int xPre = x;
+        
+            dir = aleatorio.nextInt(1,5); 
+        
+            // 1 - derecha
+            // 2 - arriba
+            // 3 - izquierda
+            // 4 - abajo
+        
+            String mov = "";
+        
+            switch(dir)
+            {
+                case 1 -> {
+                    if(x < size-1) x++; mov = "derecha";
+                }
+                case 2 -> {
+                    if(y > 0) y--;  mov = "arriba";
+                }
+                case 3 -> {
+                    if(x > 0) x--;  mov = "izquierda";
+                }
+                case 4 -> {
+                    if(y < size-1) y++; mov = "abajo";
+                }
+            }
+                        
+            if(xPre != x || yPre != y) // Hay movimiento
+            {
+                System.out.println(this.getName()+ " Me muevo " + mov + " de "+ xPre + "," +yPre + " a " + x + "," + y);
+                energy--;
+            }
+            else System.out.println(this.getName()+" NO me muevo, no se genero movimiento valido");
+               
+            
+            if(energy < initialEnergy/2) face = medium;
+            if(energy < initialEnergy/4) face = low;
+            if(energy == 0) face=dead; 
+            
+            gui.actualizarPosicion(face, xPre, yPre, x, y);
+            if(energy == 0) moving = false;
+        }
+    }
+
+    private void recharge()
+    {
+
+    }
+}
---
+++ Zumba/src/zumba/Escenario.java
@@ -0,0 +1,155 @@
+package zumba;
+
+import jade.tools.sniffer.MMCanvas;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Random;
+
+public class Escenario extends JFrame {
+    
+    private static Escenario instance;
+
+    private int matrix[][];
+    private boolean stationYN;
+    private static int stationRow = -1;
+    private static int stationColumn = -1;
+
+    private final JLabel[][] grid;
+    private final ImageIcon dirt  = new ImageIcon("Zumba/imagenes/leaf.png");
+    private final ImageIcon obstacleIcon = new ImageIcon("Zumba/imagenes/obstacle.jpg");
+    private final ImageIcon stationIcon = new ImageIcon("Zumba/imagenes/station.jpg");
+    private ImageIcon actualIcon;
+    private final BackGroundPanel floor = new BackGroundPanel(new ImageIcon("Zumba/imagenes/floor.jpg"));
+
+    private final JMenu generate = new JMenu("Random generate");
+    private final JMenu settings = new JMenu("Settings");
+    private final JRadioButtonMenuItem obstacle = new JRadioButtonMenuItem("Obstacle");
+    private final JRadioButtonMenuItem leaf = new JRadioButtonMenuItem("Leaf");
+    private final JRadioButtonMenuItem station = new JRadioButtonMenuItem("Station");
+    private final JMenuItem randomObstacle = new JMenuItem("Random Obstacle");
+    private final JMenuItem randomLeaf = new JMenuItem("Random leaves");
+
+    public static Escenario getInstance(int size, int dirtRate)
+    {
+        if(instance == null)
+           instance = new Escenario(size, dirtRate);
+        return instance;
+    }
+
+    private Escenario(int size, int dirtRate) {
+        setTitle("Probando JADE (MyName)");
+        setSize(50*size, 50*size);
+        this.setContentPane(floor);
+        setLayout(new GridLayout(size, size));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        ButtonGroup settingOptions = new ButtonGroup();
+        settingOptions.add(leaf);
+        settingOptions.add(obstacle);
+        settingOptions.add(station);
+
+        JMenuBar menuBar = new JMenuBar();
+        this.setJMenuBar(menuBar);
+        menuBar.add(settings);
+        menuBar.add(generate);
+        settings.add(station);
+        settings.add(obstacle);
+        settings.add(leaf);
+        generate.add(randomObstacle);
+        generate.add(randomLeaf);
+
+        grid = new JLabel[size][size];
+        matrix = new int[size][size];
+        Random aleatorio = new Random();
+
+        leaf.addItemListener(evt -> leafSet(evt));
+        obstacle.addItemListener(evt -> obstacleSet(evt));
+        station.addItemListener(evt -> stationSet(evt));
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                matrix[i][j] = 0;
+                grid[i][j] = new JLabel();
+                grid[i][j].setOpaque(false);
+                
+                int r = aleatorio.nextInt(0,100);
+                
+                if(r <= dirtRate)
+                    grid[i][j].setIcon(dirt);
+
+                add(grid[i][j]);
+                grid[i][j].addMouseListener(new MouseAdapter() // Este listener nos ayuda a agregar poner objetos en la rejilla
+                {
+                    @Override
+                    public void mousePressed(MouseEvent e) 
+                    {
+                               insertObject(e);
+                    }   
+                
+                    @Override
+                    public void mouseReleased(MouseEvent e) 
+                    {
+                                insertObject(e);
+                    }   
+
+
+                });
+            }
+        }
+
+    }
+
+    public void colocar(int i, int j, ImageIcon face)
+    {
+        grid[i][j].setIcon(face);
+    }
+
+    public void actualizarPosicion(ImageIcon face, int xPre, int yPre, int x, int y)
+    {
+        grid[yPre][xPre].setIcon(null);
+        grid[y][x].setIcon(face);
+    }
+
+    private void leafSet(ItemEvent eventObject){
+        JRadioButtonMenuItem opt = (JRadioButtonMenuItem) eventObject.getSource();
+        actualIcon = opt.isSelected() ? dirt : null;
+    }
+
+    private void obstacleSet(ItemEvent eventObject)
+    {
+        JRadioButtonMenuItem opt = (JRadioButtonMenuItem) eventObject.getSource();
+        actualIcon = opt.isSelected() ? obstacleIcon : null;
+    }
+
+    private void stationSet(ItemEvent eventObject)
+    {
+        JRadioButtonMenuItem opt = (JRadioButtonMenuItem) eventObject.getSource();
+        actualIcon = opt.isSelected() ? stationIcon : null;
+    }
+
+    public void insertObject(MouseEvent e){
+        JLabel box = (JLabel) e.getSource();
+        int row = (box.getY() - 15) / 50;
+        int column = (box.getX() - 15) / 50;
+
+        if(actualIcon == stationIcon && matrix[row][column] != 2){
+            if(!stationYN){
+                box.setIcon(stationIcon);
+                stationYN = true;
+                matrix[row][column] = 2;
+                stationRow = row;
+                stationColumn = column;
+            }else{
+                JOptionPane.showMessageDialog(this, "Ya existe una estación de recarga");
+            }
+        }
+        box.setIcon(actualIcon);
+        matrix[row][column] = (actualIcon == obstacleIcon) ? 1 : (actualIcon == dirt) ? 3 : 0;
+    }
+
+}
